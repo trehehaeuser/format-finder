9e2cea55f1147b1c61dedcd6a2691cfb76d2c07a
Thomas Rehehäuser <thomasrehehauser@MacBookmasM2max.fritz.box>
2025-03-31 10:19:44 +0200

    Richtiger Projektstand: mit package.json, Vite, src

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import InteraktiverFormatFinder from './InteraktiverFormatFinder'
+import './styles/formatfinder.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <InteraktiverFormatFinder />
+  </React.StrictMode>
+)
